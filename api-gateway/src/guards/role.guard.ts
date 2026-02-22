1ad4e0a828064c9d9dd7690171ae5330e3b870d5
Alessandro0003 <alessandroquinta3@gmail.com>
2026-02-22 16:57:11 -0300

    :recycle: refactor(api-gateway): removendo comentario de lint pro typescript

--- api-gateway/src/guards/role.guard.ts
+++ api-gateway/src/guards/role.guard.ts
@@ -20,15 +20,12 @@ export class RoleGuard implements CanActivate {
       return true; // No role required, allow access
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     const user = context.switchToHttp().getRequest().user;
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     if (!user || !user.role) {
       throw new ForbiddenException('User role not found'); // No user or roles found, deny access
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
     const hasRole = requiredRole.includes(user.role);
 
     if (!hasRole) {
