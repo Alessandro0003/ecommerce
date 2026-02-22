005f05e3eef3cd9212168eefd2f2393e1610132c
Alessandro0003 <alessandroquinta3@gmail.com>
2026-02-21 23:22:46 -0300

    :recycle: refactor(api-gateway): removendo no-unsafe-member-acces

--- api-gateway/src/auth/decorators/current-user.decorator.ts
+++ api-gateway/src/auth/decorators/current-user.decorator.ts
@@ -1,16 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-interface CurrentUserData {
-  user: {
-    id: string;
-    email: string;
-    roles: string[];
-  };
-}
-
 export const CurrentUser = createParamDecorator(
   (_data: any, ctx: ExecutionContext) => {
-    const request: CurrentUserData = ctx.switchToHttp().getRequest();
+    const request = ctx.switchToHttp().getRequest();
     return request.user;
   },
 );
