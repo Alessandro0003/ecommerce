dd1363d010fd38fe7a2901d02074b7e38d919e6b
Alessandro0003 <alessandroquinta3@gmail.com>
2026-02-25 21:26:59 -0300

    :construction: feat(api-gateway): criando setup pro CircuitBreaker

---
+++ api-gateway/src/common/circuit-breaker/circuit-breaker.interface.ts
@@ -0,0 +1,25 @@
+enum CircuitBreakerStateEnum {
+  CLOSED = 'CLOSED',
+  OPEN = 'OPEN',
+  HALF_OPEN = 'HALF_OPEN',
+}
+
+export interface CircuitBreakerOptions {
+  failureThreshold: number; // Numero de falhas consecutivas para abrir o circuito
+  timeout: number; // Tempo em milissegundos para considerar uma chamada como falha
+  resetTimeout: number; // Tempo em milissegundos para tentar fechar o circuito novamente
+}
+
+export interface CircuitBreakerState {
+  state: CircuitBreakerStateEnum;
+  failureCount: number; // Contador de falhas consecutivas
+  lastFailureTime: number; // Timestamp da última falha
+  nextAttemptTime: number; // Timestamp para a próxima tentativa de fechar o circuito
+}
+
+export interface CircuitBreakerResult<T> {
+  success: boolean; // Indica se a chamada foi bem-sucedida ou não
+  data?: T; // Dados retornados pela chamada, se houver
+  error?: Error; // Erro ocorrido durante a chamada, se houver
+  fromCache?: boolean; // Indica se o resultado foi retornado do cache (em caso de circuito aberto)
+}
