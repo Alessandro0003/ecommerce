be02b92911aada931c1696c14b7e08c85bf6cffa
Alessandro0003 <alessandroquinta3@gmail.com>
2026-03-16 22:00:03 -0300

    :sparkles: feat: implementando methods para nosso service de health

--- api-gateway/src/health/health.service.ts
+++ api-gateway/src/health/health.service.ts
@@ -1,10 +1,66 @@
 import { Injectable } from '@nestjs/common';
+import { HealthStatus } from 'src/common/health/health-check.interface';
+import { HealthCheckService } from 'src/common/health/health-check.service';
 
 @Injectable()
 export class HealthService {
-  constructor() {}
+  constructor(private readonly healthCheckService: HealthCheckService) {}
 
-  getReadyStatus() {}
+  // Realiza o health check de todos os serviços e do próprio gateway
+  async getHealthStatus() {
+    const healthChecks = await this.healthCheckService.checkAllServices();
+    const results = {
+      status: HealthStatus.HEALTHY,
+      timestamp: new Date().toISOString(),
+      gateway: {
+        status: HealthStatus.HEALTHY,
+        uptime: process.uptime(),
+        memoryUsage: process.memoryUsage(),
+      },
+      services: {},
+    };
 
-  getLiveStatus() {}
+    let hasUnhealthyServices = false;
+
+    // Processa os resultados dos health checks dos serviços
+    healthChecks.forEach((servicesHealth) => {
+      results.services[servicesHealth.name] = {
+        status: servicesHealth.status,
+        responseTime: servicesHealth.responseTime,
+        lastChecked: servicesHealth.lastChecked,
+        url: servicesHealth.url,
+        ...(servicesHealth.error && { error: servicesHealth.error }),
+      };
+
+      if (servicesHealth.status === HealthStatus.UNHEALTHY) {
+        hasUnhealthyServices = true;
+      }
+    });
+
+    if (hasUnhealthyServices) {
+      results.status = HealthStatus.DEGRADED;
+    }
+
+    return results;
+  }
+
+  // Verifica para ver se o gateway está pronto para receber tráfego (ex: conexões com bancos, serviços essenciais)
+  async getReadyStatus() {
+    const healthStatus = await this.getHealthStatus();
+    return {
+      stauts:
+        healthStatus.status === HealthStatus.HEALTHY ? 'ready' : 'not ready',
+      timestamp: new Date().toISOString(),
+    };
+  }
+
+  // Verifica se o gateway está vivo (ex: processo rodando, sem erros críticos)
+  async getLiveStatus() {
+    const healthStatus = await this.getHealthStatus();
+    return {
+      status:
+        healthStatus.status === HealthStatus.HEALTHY ? 'alive' : 'not alive',
+      timestamp: new Date().toISOString(),
+    };
+  }
 }
