04d788433ef8a20cc7c97d0e3657f033745a8186
Alessandro0003 <alessandroquinta3@gmail.com>
2026-04-05 21:04:30 -0300

    :wrench: chore: habilitando cors e passando as variaveis de ambiente tipadas

--- checkout-service/src/main.ts
+++ checkout-service/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from './env';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(env.PORT);
+
+  console.log(`🚀 Checkout Service is running in http://localhost:${env.PORT}`);
 }
 bootstrap();
