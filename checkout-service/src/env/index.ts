e237934615ac85c05eb909d7fe3254abf70a903d
Alessandro0003 <alessandroquinta3@gmail.com>
2026-04-05 21:03:54 -0300

    :sparkles: feat: adicionando e tipando variaveis de ambiente

--- checkout-service/src/env/index.ts
+++ checkout-service/src/env/index.ts
@@ -7,7 +7,17 @@ config({ path: resolve(__dirname, '../../.env') });
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']),
+  PORT: z.coerce.number().default(3003),
   DATEBASE_URL: z.string().url(),
+  JWT_SECRET: z.string().min(1),
+  JWT_EXPIRES_IN: z.string().min(1),
+  USERS_SERVICE_URL: z.string().url(),
+  PAYMENTS_SERVICE_URL: z.string().url(),
+  PRODUCTS_SERVICE_URL: z.string().url(),
+  API_GATEWAY_URL: z.string().url(),
+  RABBITMQ_URL: z.string().url(),
+  RABBITMQ_QUEUE_PAYMENTS: z.string().min(1),
+  RABBITMQ_EXCHANGE: z.string().min(1),
 });
 
 export const env = envSchema.parse(process.env);
