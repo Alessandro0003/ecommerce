1890b08bb24576d63b5f97c9b6e388e26dfd7460
Alessandro0003 <alessandroquinta3@gmail.com>
2026-05-09 17:24:08 -0300

    :sparkles: feat: subscribeToQueue

--- checkout-service/src/events/rabbitmq/rabbitmq.service.ts
+++ checkout-service/src/events/rabbitmq/rabbitmq.service.ts
@@ -123,4 +123,66 @@ export class RabbitmqService implements OnModuleInit, OnModuleDestroy {
       );
     }
   }
+
+  async subscribeToQueue(
+    queueName: string,
+    exchange: string,
+    routineKey: string,
+    callback: (message: unknown) => Promise<void>,
+  ) {
+    try {
+      if (!this.channel) {
+        this.logger.warn(
+          '⚠️ RabbitMQ channel is not available, cannot subscribe to queue',
+        );
+        return;
+      }
+
+      await this.channel.assertExchange(exchange, 'topic', { durable: true });
+
+      const queue = await this.channel.assertQueue(queueName, {
+        durable: true,
+        arguments: {
+          'x-message-ttl': 86400000, // Define o tempo de vida das mensagens na fila (24 horas)
+          'x-max-length': 10000, // Limita o número máximo de mensagens na fila
+        },
+      });
+
+      await this.channel.bindQueue(queue.queue, exchange, routineKey);
+
+      await this.channel.prefetch(1); // Processa uma mensagem por vez para garantir a ordem
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      await this.channel.consume(queue.queue, async (msg) => {
+        if (msg) {
+          try {
+            const messageContent: unknown = JSON.parse(msg.content.toString());
+            this.logger.log(
+              `✅ Received message from RabbitMQ from queue ${queueName}`,
+            );
+            this.logger.debug(
+              `Message content: ${JSON.stringify(messageContent)}`,
+            );
+            await callback(messageContent);
+
+            this.channel?.ack(msg); // Confirma que a mensagem foi processada com sucesso
+
+            this.logger.log(
+              `✅ Message processed successfully from queue: ${queueName}`,
+            );
+          } catch (error) {
+            this.logger.error(
+              '❌ Error processing message from RabbitMQ',
+              error instanceof Error ? error.message : String(error),
+            );
+            this.channel?.nack(msg, false, false); // !TODO: Dead Letter Queue
+          }
+        }
+      });
+    } catch (error) {
+      this.logger.error(
+        '❌ Error subscribing to RabbitMQ queue',
+        error instanceof Error ? error.message : String(error),
+      );
+    }
+  }
 }
