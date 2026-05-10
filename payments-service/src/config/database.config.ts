7ea6f76a9126364e0b679cf62d334b227b34bf8b
Alessandro0003 <alessandroquinta3@gmail.com>
2026-05-10 20:55:09 -0300

    :wrench: chore: passando url de conexao do banco

--- payments-service/src/config/database.config.ts
+++ payments-service/src/config/database.config.ts
@@ -1,5 +1,8 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { env } from '../env';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
+  url: env.DATABASE_URL,
+  // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
 };
