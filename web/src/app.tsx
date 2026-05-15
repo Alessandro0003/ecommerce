b775a86f76f9c86a25fd0ce64a19527d5df7781f
Alessandro0003 <alessandroquinta3@gmail.com>
2026-05-14 23:26:18 -0300

    :sparkles: feat: criando themaToggle

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,7 +1,11 @@
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
+
 export function App() {
   return (
-    <>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       <h1>Hello World</h1>
-    </>
+      <Toaster />
+    </ThemeProvider>
   );
 }
