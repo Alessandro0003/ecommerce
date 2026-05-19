660d751dbe906241c5d338c750e1210af040d1e4
Alessandro0003 <alessandroquinta3@gmail.com>
2026-05-18 23:43:20 -0300

    :sparkles: feat: implementando service e controller

--- payments-service/src/events/events.module.ts
+++ payments-service/src/events/events.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { RabbitmqService } from './rabbitmq/rabbitmq.service';
 import { PaymentQueueService } from './payment-queue/payment-queue.service';
 import { PaymentConsumerService } from './payment-consumer/payment-consumer.service';
+import { DlqService } from './dlq/dlq.service';
+import { DlqController } from './dlq/dlq.controller';
 
 @Module({
-  providers: [RabbitmqService, PaymentQueueService, PaymentConsumerService],
+  providers: [
+    RabbitmqService,
+    PaymentQueueService,
+    PaymentConsumerService,
+    DlqService,
+  ],
+  controllers: [DlqController],
 })
 export class EventsModule {}
